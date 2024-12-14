8d55b807a6e93ba0185162c4a818be72e9264741
Kasperi Jaakkola <kassuvaa@gmail.com>
2024-12-14 19:54:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Scoreboards
+Scoreboards is an easy to use score keeper application accessible to everyone on the web. 
+
+React/Tailwind experiencement project
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-center">Scoreboards</h1>
+    </div>
+  );
+}
+
+export default App;
